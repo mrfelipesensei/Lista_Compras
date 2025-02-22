f044cf2a7f126992774f933e45437fa4e509c33d
Felipe Greg <mrfelipesensei@gmail.com>
2025-02-22 13:56:10 -0300

    adicionar_item()

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 #Lista de compras (dicionário: item -> [quantidade, preço])
+lista_compras = {}
+
+#Função de adicionar item
+def adicionar_item():
+    item = input("Digite o nome do item: ").strip().lower()
+    quantidade = int(input("Digite a quantidade: "))
+    preco = float(input("Digite o preço unitário: "))
+
+    #Lógica de verificação e adição do item à lista
+    if item in lista_compras:
+        lista_compras[item][0] += quantidade #Atualiza quantidade
+    else:
+        lista_compras[item] = [quantidade,preco]
+
+    print(f"{quantidade}x {item} adicionado(s) à lista de compras.")
